801c767141db64e75ecbb5a860dd242fe8025c02
wsrn829 <wsrn829@gmail.com>
2023-12-20 17:48:20 -0500

    Navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,32 @@ import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
+
     <div className={`&{styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-          <Navbar />
+        <Navbar />
       </div>
-
-      <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Hero />
-        </div>
-      </div>
-
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Stats />
-          <Business />
-          <Billing />
-          <CardDeal />
-          <Testimonials />
-          <Clients />
-          <CTA />
-          <Footer />
-        </div>
-      </div>
-
     </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+
   </div>
 )
 
